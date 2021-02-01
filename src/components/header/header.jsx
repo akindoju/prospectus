707f7640c907b80f1a0dc36d34afef1deb587214
Akndju <davidd.akindoju@gmail.com>
2021-02-01 15:29:30 +0100

    Created Header Component

---
+++ src/components/header/header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+  return (
+    <div>
+      <div className="nav">
+        <ul className="listContainer">
+          <li>Home</li>
+          <li>Courses</li>
+          <li>Contact Info</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
