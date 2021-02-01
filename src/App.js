3a2c6e0049bb933e46bace74f706f403ee720ab5
Akndju <davidd.akindoju@gmail.com>
2021-02-01 15:48:49 +0100

    Created Homepage Component and moved Header component in

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from './components/header/header';
+import Homepage from './components/homepage/homepage';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Homepage />
     </div>
   );
 }
