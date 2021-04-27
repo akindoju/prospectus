f49887f423a35c7c396b19f25c9f66c1bd66111e
Akndju <davidd.akindoju@gmail.com>
2021-04-27 13:53:32 +0100

    Styled levelsection

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import About from './components/About/About';
+import LevelSection from './components/LevelSection/LevelSection';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <About />
+      <LevelSection />
     </div>
   );
 }
