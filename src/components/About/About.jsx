ac8e9628b9966e5f11292198e8cfa152cff7b299
Akndju <davidd.akindoju@gmail.com>
2021-05-04 09:18:38 +0100

    Edited about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,12 +11,10 @@ const About = () => {
       </p>
 
       <p class="paragraph">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi quisquam
-        quasi accusamus hic consequatur repudiandae, beatae incidunt provident,
-        vel velit debitis iste, in voluptatum eum. Facilis, placeat. Placeat
-        optio atque, excepturi quisquam soluta esse doloribus aliquam facilis?
-        Modi numquam, quod dignissimos aperiam dolor nesciunt distinctio
-        expedita repellendus nostrum, quis fuga?
+        The four cards below represent each year of the Computer science
+        programme. Each card contains first semester and second semester
+        courses, and may also include compulsory courses for direct entry
+        students and also elective courses.
       </p>
     </section>
   );
